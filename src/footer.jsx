634c14e9831c5e325e63c20849950965fbba40d4
Ansh Mathur <worldanvilbild@gmail.com>
2024-11-23 15:51:19 +0530

    *

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,11 +1,11 @@
 import { Helmet } from "react-helmet"
 import { Plane } from "lucide-react"
+
 export default ()=>{
     return (
         <header className="bg-primary text-primary-foreground">
             <Helmet>
                 <script src="https://cdn.tailwindcss.com"></script>
-                <link rel="icon" type="image/png" href="/favicon.ico"/>
             </Helmet>
         <div className="container mx-auto px-4 py-6 flex justify-between items-center">
         <a href="/" className="flex items-center space-x-2">
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from 'path'
+import { faviconsPlugin } from '@darkobits/vite-plugin-favicons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteStaticCopy({
+  plugins: [react(), faviconsPlugin({
+    icons: {
+      favicons: {
+        source: './favicon.ico'
+      }
+    }
+  }), viteStaticCopy({
     targets: [
       {src: "src/assets/marker-icon-2x.png", dest: "./assets"}
     ]
